86e4d795cf5e7f767b529cd968250e0be350d800
Yash Jadhav <ygjadhav04@gmail.com>
2026-06-18 15:54:48 +0530

    refactor: remove temporary pybind test bindings

--- algorithm/bindings/pybind_wrapper.cpp
+++ algorithm/bindings/pybind_wrapper.cpp
@@ -9,61 +9,6 @@
 
 namespace py = pybind11;
 
-int add(int a, int b)
-{
-    return a + b;
-}
-
-std::unordered_map<int, double> test_balances()
-{
-    Expense expense;
-
-    expense.expense_id = 1;
-    expense.group_id = 1;
-    expense.paid_by = 1;
-    expense.amount = 1000.0;
-
-    expense.participants = {
-        {1, 500.0},
-        {2, 300.0},
-        {3, 200.0}
-    };
-
-    return computeNetBalances({expense});
-}
-
-std::vector<std::unordered_map<std::string, double>>
-test_settlement()
-{
-    Expense expense;
-
-    expense.expense_id = 1;
-    expense.group_id = 1;
-    expense.paid_by = 1;
-    expense.amount = 1000.0;
-
-    expense.participants = {
-        {1, 500.0},
-        {2, 300.0},
-        {3, 200.0}
-    };
-
-    auto transactions = minimizeTransactions({expense});
-
-    std::vector<std::unordered_map<std::string, double>> result;
-
-    for (const auto& t : transactions)
-    {
-        result.push_back({
-            {"from_user_id", static_cast<double>(t.from_user_id)},
-            {"to_user_id", static_cast<double>(t.to_user_id)},
-            {"amount", t.amount}
-        });
-    }
-
-    return result;
-}
-
 std::vector<std::unordered_map<std::string, double>>
 minimize_transactions(py::list expenses_py)
 {
@@ -129,24 +74,6 @@ minimize_transactions(py::list expenses_py)
 PYBIND11_MODULE(debt_optimizer, m)
 {
     m.doc() = "FundyWise Debt Optimization Module";
-
-    m.def(
-        "add",
-        &add,
-        "Add two integers"
-    );
-
-    m.def(
-        "test_balances",
-        &test_balances,
-        "Return sample balances"
-    );
-
-    m.def(
-        "test_settlement",
-        &test_settlement,
-        "Return sample settlement transactions"
-    );
 
     m.def(
         "minimize_transactions",
